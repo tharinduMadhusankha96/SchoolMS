87db0109bbc0c57489cd3b128d08d37a65c562d0
Rashmika Opatha <42743915+rashmika97@users.noreply.github.com>
2018-09-25 08:02:21 +0530

    Update Society.php

--- app/Society.php
+++ app/Society.php
@@ -37,4 +37,9 @@ class Society extends Model
         return $this->belongsToMany(User::class);
     }
 
+    public function events()
+    {
+        return $this->hasMany(Event::class);
+    }
+
 }
