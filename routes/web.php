7f6d85ff2ed9b14b41111b8aa47f0663b76a93d3
Rashmika Opatha <42743915+rashmika97@users.noreply.github.com>
2018-09-25 08:46:09 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,15 +12,28 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Auth::routes();
 
+Route::post("/adminDashboard" , 'CustomLoginController@login');
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['middleware' => 'auth'] , function () {
+
+    Route::get("/adminDashboard", function () {
+        return view('adminDashboard');
+    });
+});
+
+Route::get('/home', 'HomeController@index');
+Route::get('/createAdmin' , 'CustomLoginController@createAdmin');
+Route::post('/storeAdmin' , 'CustomLoginController@storeAdmin');
+Route::get('/allAdmins' , 'CustomLoginController@admins');
+Route::delete('/Admindestroy/{admin} ' , 'CustomLoginController@Admindestroy');
 
 
+//Event Routes
 Route::get('Event/search','EventController@search');
 Route::get('Event/createDemo','EventController@createDemo');
 Route::get('Event/monthlyEvent', 'EventController@monthlyEvent');
@@ -30,22 +43,14 @@ Route::post('/Event/showEvent/{event}', 'EventController@updateImage');
 Route::get('/Event/calendar', 'EventController@calendar');
 Route::resource('Event' , 'EventController');
 
-
-
-//Route::post('Event/{event}/comment/{comment}','CommentsController@destroy');
 Route::post('Event/{event}/comment','CommentsController@store');
 Route::get('Sport/search','SportController@search');
 Route::get('Sport/createDemo','SportController@createDemo');
 Route::get('Sport/enrolledStudents' , 'SportController@enrolledStudents');
 Route::get('Sport/mysports','SportController@mysports');
-
-
-
 Route::post('Sport/addStudent' , 'SportController@addStudent');
 Route::delete('Sport/removeStudent' , 'SportController@removeStudent');
 Route::resource('Sport' ,'SportController');
-
-
 
 Route::get('Society/search','SocietyController@search');
 Route::get('Society/createDemo','SocietyController@createDemo');
@@ -55,14 +60,20 @@ Route::post('Society/addStudent' , 'SocietyController@addStudent');
 Route::delete('Society/removeStudent' , 'SocietyController@removeStudent');
 Route::resource('Society' , 'SocietyController');
 
+Route::get('eventAdmin' , 'EventAdminController@eventAdmin');
+Route::get('societyAdmin' , 'EventAdminController@societyAdmin');
+Route::get('sportAdmin' , 'EventAdminController@sportAdmin');
+//End of Event Routes
+
+
 //inventory
 Route::resource('/orders','Ordercontroller');
 Route::resource('/supplier','Suppliercontroller');
 Route::resource('/stationary','Stationarycontroller');
 Route::resource('/labs','labscontroller');
-Route::resource('/sports','Sportscontroller');
+Route::resource('/inventorysports','Sportstocks');
 Route::resource('/resource','Resourcecontroller');
-Route::resource('/expenses','InventoryExpenses');
+Route::resource('/inventoryexpenses','Inventoryexpenses');
 Route::get('/inventory','homeview@index');
 Route::get('/live_search', 'LiveSearch@index');
 Route::get('/live_search/action', 'LiveSearch@action')->name('live_search.action');
@@ -77,13 +88,9 @@ Route::resource('leaverequests','LeaveRequestController');
 Route::resource('leaverequestsnon','LeaveRequestNonController');
 
 
-
-
 //student
 Route::post('/update', 'studentController@store');
 Route::get('/std', 'studentController@show');
 Route::get('/delete', 'studentController@destroy');
 Route::get('/fee', 'studentController@store');
 Route::get('/home', 'HomeController@index')->name('home');
-
-
