1c5ec168512b336692b091a96a3a361d82ad2d5a
Rashmika Opatha <42743915+rashmika97@users.noreply.github.com>
2018-09-25 07:38:21 +0530

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/Home';
+    protected $redirectTo = '/home';
 
     /**
      * Create a new controller instance.
