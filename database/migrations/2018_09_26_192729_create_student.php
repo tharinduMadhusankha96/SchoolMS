7dc5ba11f229fba245bc59166c59842e9b71a023
shirantha97 <39570216+shirantha97@users.noreply.github.com>
2018-09-28 22:52:23 +0530

    inventory

---
+++ database/migrations/2018_09_26_192729_create_student.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudent extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+
+    public function up()
+    {
+        Schema::create('student', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('studentName');
+            $table->string('guardianName');
+            $table->string('permanetAddress');
+            $table->string('currentAddress');
+            $table->string('Gender');
+            $table->string('Province');
+            $table->string('Grade');
+            $table->string('BirthDay');
+            $table->string('mobileNumber');
+            $table->string('email');
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student');
+    }
+}
